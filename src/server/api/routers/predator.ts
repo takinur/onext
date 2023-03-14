2061251eef4dfe77119dbbe93dd3a462383675d2
Takinur M <takinurm@gmail.com>
2023-03-14 15:47:16 +0600

    Public to Private Procedure

--- src/server/api/routers/predator.ts
+++ src/server/api/routers/predator.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const predatorRouter = createTRPCRouter({
-  postMessage: publicProcedure
+  postMessage: protectedProcedure
     .input(
       z.object({
         name: z.string(),
