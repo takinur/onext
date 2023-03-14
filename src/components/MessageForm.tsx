02def61f8663dc93ee2381ccf344d37ee4c17765
Takinur M <takinurm@gmail.com>
2023-03-14 16:27:46 +0600

    Added Form to Index

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -1,4 +1,4 @@
-import { type FC, useState } from "react";
+import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 
@@ -22,4 +22,34 @@ export const MessageForm = () => {
       await utils.predator.getMessages.invalidate();
     },
   });
+
+  return status === "authenticated" ? (
+    <form
+      className="flex gap-2"
+      onSubmit={(event) => {
+        event.preventDefault();
+        postMessage.mutate({
+          name: sessionData.user?.name as string,
+          message,
+        });
+        setMessage("");
+      }}
+    >
+      <input
+        type="text"
+        className="rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none"
+        placeholder="Your message..."
+        minLength={2}
+        maxLength={200}
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-md border-2 border-zinc-800 p-2 focus:outline-none"
+      >
+        Submit
+      </button>
+    </form>
+  ) : null;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { PredatorEntries } from "~/components/PredatorEntries";
-
+import { MessageForm } from "~/components/MessageForm";
 
 const Home: NextPage = () => {
   return (
@@ -41,7 +41,12 @@ const AuthShowcase: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {sessionData && (
+          <div className="w-full">
+            <span>Logged in as {sessionData.user?.name}</span>
+            <MessageForm />{" "}
+          </div>
+        )}
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
@@ -55,4 +60,3 @@ const AuthShowcase: React.FC = () => {
     </div>
   );
 };
-
