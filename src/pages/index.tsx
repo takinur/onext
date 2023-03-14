48a4d28b2e536ce45179e8508952c3383b4f2022
Takinur M <takinurm@gmail.com>
2023-03-14 16:32:12 +0600

    Changed Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Violette <span className="text-[hsl(280,100%,70%)]">NextJS</span>{" "}
+            Discord Auth <span className="text-[hsl(280,100%,70%)]">Chat</span>{" "}
             App
           </h1>
 
